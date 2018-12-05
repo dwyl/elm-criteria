4025ac4abd311c6f9ee3285a0e5ff48e08921828
SimonLab <simon.labondance@gmail.com>
2018-12-05 04:15:53 +0000

    update config to get filter name and sub-filters, #4

--- examples/Example.elm
+++ examples/Example.elm
@@ -1,5 +1,5 @@
 import Browser
-import Html exposing (Html, button, div, h1, text)
+import Html exposing (Html, button, div, h1, text, p)
 import Html.Attributes exposing (..)
 import Criteria
 
@@ -15,12 +15,14 @@ type alias Model = {criteria : Criteria.State}
 init : Model
 init = {criteria = Criteria.init}
 
-criteriaConfig : Criteria.Config Msg
+criteriaConfig : Criteria.Config Msg Filter
 criteriaConfig =
-    Criteria.config { toMsg = UpdateCriteria }
+    Criteria.config { toMsg = UpdateCriteria, getFilterName = getFilterName, getSubFilters = getSubFilters }
 -- UPDATE
 
 type Msg = UpdateCriteria Criteria.State
+
+type alias Filter = (String, List String)
 
 update : Msg -> Model -> Model
 update msg model =
@@ -33,5 +35,17 @@ view : Model -> Html Msg
 view model =
   div []
     [ h1 [] [text "Criteria Package"]
-    , Criteria.view criteriaConfig model.criteria
+    , Criteria.view criteriaConfig model.criteria filters
+    , p [] [text "this is the rest of the application content"]
     ]
+
+
+filters : List Filter
+filters = [("filter1", ["filter11", "filter12", "filter13"]), ("filter2", ["filter21", "filter22"]), ("filter3", []), ("filter4", ["filter41"])]
+
+getFilterName : (String, List String) -> String
+getFilterName (filter, _) =
+    filter
+
+getSubFilters : Filter -> List String
+getSubFilters (_, subFilters) = subFilters
--- src/Criteria.elm
+++ src/Criteria.elm
@@ -6,19 +6,49 @@ import Json.Decode as Json
 
 type State = State Bool
 
-type Config msg = Config {toMsg : State -> msg}
+type Config msg filter = Config {toMsg : State -> msg, getFilterName : filter -> String, getSubFilters : filter -> List String}
 
 init: State
 init = State False
 
-config : {toMsg: State -> msg} -> Config msg
-config {toMsg} = Config {toMsg = toMsg}
+config :
+  { toMsg: State -> msg
+  , getFilterName : filter -> String
+  , getSubFilters : filter -> List String
+  } -> Config msg filter
+config {toMsg, getFilterName, getSubFilters} =
+  Config {toMsg = toMsg, getFilterName = getFilterName, getSubFilters = getSubFilters}
 
-view : Config msg -> State -> Html msg
-view (Config {toMsg}) (State open) =
+
+
+viewSubFilters : List String -> Html msg
+viewSubFilters subFilters =
+  case subFilters of
+    [] -> span [] []
+    _ -> div [] <| List.map (\subFilter -> viewFilter subFilter []) subFilters
+
+viewFilter : String -> List String -> Html msg
+viewFilter filter subFilters =
+  div [style "margin-left" "20px"] [
+    label []
+      [
+        input [type_ "checkbox"] []
+      , text filter
+      , viewSubFilters subFilters
+      ]
+    ]
+
+
+viewFilters: (filter -> String) -> (filter -> List String) -> List filter -> Html msg
+viewFilters getFilterName getSubFilters filters =
+  div [] <| List.map (\f -> viewFilter (getFilterName f) (getSubFilters f)) filters
+
+
+view : Config msg filter -> State -> List filter -> Html msg
+view (Config {toMsg, getFilterName, getSubFilters}) (State open) filters =
   div []
     [ button [onClick (State (not open)) toMsg ] [text "Open/Close Filters"]
-    , div [style "display" (if open then "block" else "none")] [ text "list of filters"]
+    , div [style "display" (if open then "block" else "none")] [viewFilters getFilterName getSubFilters filters]
     ]
 
 onClick : State -> (State -> msg) -> Attribute msg
