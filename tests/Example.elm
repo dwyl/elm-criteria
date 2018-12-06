b277727cd72636fd31e9030e27ac9c8e15b69266
SimonLab <simon.labondance@gmail.com>
2018-12-06 01:42:39 +0000

    create dummy test to see if Travis is triggered, #4

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,4 +1,4 @@
-module Example exposing (..)
+module Example exposing (suite)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
@@ -7,4 +7,15 @@ import Test exposing (..)
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    describe "TESTING"
+        [ describe "String.reverse"
+            -- Nest as many descriptions as you like.
+            [ test "has no effect on a palindrome" <|
+                \_ ->
+                    let
+                        palindrome =
+                            "hannah"
+                    in
+                    Expect.equal palindrome (String.reverse palindrome)
+            ]
+        ]
